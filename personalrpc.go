2d7d354aaf555a764462c1fbf5e1886524037651
johnsneakers <362380376@qq.com>
2018-01-12 16:01:17 +0800

    new ethereum server

--- personalrpc.go
+++ personalrpc.go
@@ -2,7 +2,7 @@ package ethrpc
 
 func (rpc *EthRPC) Personal_newAccount(password string) (string, error) {
 	var hash string
-	pwd := []string{password}
-	err := rpc.call("personal_newAccount", &hash, pwd)
+	//pwd := []string{password}
+	err := rpc.call("personal_newAccount", &hash, password)
 	return hash, err
 }
